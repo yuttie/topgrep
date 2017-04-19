f1c7caf77e0a09b2e5302ec02f0bca874fb8f972
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-04-19 10:24:31 +0900

    Parse into a Hash and yield it

--- test.rb
+++ test.rb
@@ -1,23 +1,36 @@
 #!/usr/bin/ruby
 
-PID = 129175
+def parse(stream)
+  Enumerator.new do |y|
+    while line = stream.gets
+      snapshot = {}
 
-while line = ARGF.gets
-  line.chomp!
-  if line =~ /^top - (.+?) up/
-    time = $1
-    line = ARGF.gets.chomp until line == ''
-    col_names = ARGF.gets.chomp.split(/\s+/).values_at(1..-1)
-    while line = ARGF.gets
       line.chomp!
-      break if line == ''
-      line.strip!
-      values = line.split(/\s+/, col_names.size).map {|x| x.strip}
-      printf("%s\t)
-      if col_names.length != values.length
-        p(col_names)
-        p(values)
+      if line =~ /^top - (.+?) up/
+        time = $1
+        snapshot[:time] = time
+        snapshot[:procs] = []
+
+        line = stream.gets.chomp until line == ''
+
+        col_names = stream.gets.chomp.split(/\s+/).values_at(1..-1)
+        while line = stream.gets
+          line.chomp!
+          line.strip!
+          break if line == ''
+
+          values = line.split(/\s+/, col_names.size).map {|x| x.strip}
+          snapshot[:procs] << Hash[col_names.map {|n| n.delete('^0-9a-zA-Z').downcase.to_sym }.zip(values)]
+        end
+        y << snapshot
       end
     end
   end
+end
+
+PID = 129175
+
+parse(ARGF).each do |s|
+  p s[:time]
+  p s[:procs][0]
 end
